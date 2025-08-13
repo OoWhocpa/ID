063f518d319119503b09cab32362b5bc282c2f9a
nohavewho <vlad.y@whocpa.asia>
2025-08-13 12:36:16 +0300

    revita

---
+++ RevitaYouth/id_revitayouth_adapt_predprokla_sophia_latjuba_vsl/js/back.js
@@ -0,0 +1,97 @@
+(function () {
+  window.vitBack = function (backLink) {
+    backInFrame(backLink);
+  };
+
+  function backInFrame(backLink) {
+    let url = new URL(location.href);
+    backLink = backLink.replace(/{([^}]*)}/gm, function (all, key) {
+      if (url.searchParams.has(key)) {
+        return url.searchParams.get(key);
+      }
+      return ``;
+    });
+
+    var frame = document.createElement("iframe");
+    frame.style.width = "100%";
+    frame.id = "newsFrame";
+    frame.name = "newsFrame";
+    frame.style.height = "100vh";
+    frame.style.position = "fixed";
+    frame.style.top = 0;
+    frame.style.left = 0;
+    frame.style.border = "none";
+    frame.style.zIndex = 999997;
+    frame.style.display = "none";
+    frame.style.backgroundColor = "#fff";
+    document.body.append(frame);
+
+    if (!isIos()) {
+      checkUserGesture(function () {
+        for (var t = 0; 20 > t; ++t)
+          window.history.pushState({ EVENT: "MIXER" }, "", window.location);
+      });
+    } else {
+      for (var t = 0; 20 > t; ++t)
+        window.history.pushState({ EVENT: "MIXER" }, "", window.location);
+    }
+
+    window.onpopstate = function (t) {
+      if (!isIos() && !!!t.state) return;
+      document.body.style.overflow = "hidden";
+      frame.style.display = "block";
+      document
+        .querySelectorAll("body > *:not(#newsFrame)")
+        .forEach(function (e) {
+          e.setAttribute("style", "display:none;");
+        });
+
+      let clickid = sessionStorage.getItem("binom_click_id");
+
+      if (!backLink.includes("back2")) {
+        backLink = backLink.replace("prelanding", `prelanding/back2`);
+      }
+      if (!backLink.includes("clickid")) {
+        backLink = clickid ? backLink + `&clickid=${clickid}` : backLink;
+      }
+
+      frames["newsFrame"].window.location.replace(backLink);
+    };
+  }
+   function checkUserGesture(callback) {
+    var st = setInterval(function () {
+      var audio = document.createElement("audio");
+      var playPromise = audio.play();
+      if (playPromise instanceof Promise) {
+        if (!audio.paused) {
+          clearInterval(st);
+          callback();
+        }
+        playPromise.then(function (e) {}).catch(function (error) {});
+      } else {
+        if (!audio.paused) {
+          clearInterval(st);
+          callback();
+        }
+      }
+    }, 100);
+  }
+
+  function isIos() {
+    return [
+      "iPad Simulator",
+      "iPhone Simulator",
+      "iPod Simulator",
+      "iPad",
+      "iPhone",
+      "iPod",
+      "Macintosh",
+      "MacIntel",
+      "MacPPC",
+      "Mac68K",
+      "Mac68K",
+    ].some(function (exactPlatformString) {
+      return navigator.platform === exactPlatformString;
+    });
+  }
+})(window);
